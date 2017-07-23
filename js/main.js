28367897a1676768d5993864a77c33886a3e8ef0
Andre Reus <andrereus@users.noreply.github.com>
2017-07-23 15:19:45 +0200

    New version warning

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@
 $("#storage").on("click", function(){
     $("#dropdown").toggleClass("hidden");
 });
+
+/* Redirect */
+var conf = confirm("Sorry, the new version is currently not working. I recommend using the old version. Redirect to old version?");
+if (conf == true) {
+    window.location.assign("old/index.html");
+}
