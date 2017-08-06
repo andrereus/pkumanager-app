af5f15f5432b532962bcfb5ecae8464b19016b53
Andre Reus <andrereus@users.noreply.github.com>
2017-08-06 19:21:08 +0200

    Startup fix

--- js/login.js
+++ js/login.js
@@ -65,8 +65,21 @@ function initApp() {
     document.getElementById('sign-in').addEventListener('click', toggleSignIn, false);
 }
 
-// Phonegap specific
-document.addEventListener("deviceready", onDeviceReady, false);
+document.onreadystatechange = function() {
+    if (document.readyState === "complete") {
+        window.isphone = false;
+        if (document.URL.indexOf("http://") == -1 && document.URL.indexOf("localhost") != 7) {
+            window.isphone = true;
+        }
+
+        if (window.isphone) {
+            document.addEventListener("deviceready", onDeviceReady, false);
+        } else {
+            onDeviceReady();
+        }
+    }
+};
+
 function onDeviceReady() {
     initApp();
 }
