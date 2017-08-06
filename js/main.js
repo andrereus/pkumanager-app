755aa07181a025c8f99675b54676beb411093ddb
Andre Reus <andrereus@users.noreply.github.com>
2017-08-06 22:30:35 +0200

    Fix storage problem

--- js/main.js
+++ js/main.js
@@ -2,9 +2,3 @@
 $("#storage").on("click", function(){
     $("#dropdown").toggleClass("hidden");
 });
-
-/* Redirect */
-var conf = confirm("Sorry, the new version is currently not working. I recommend using the old version. Use old version?");
-if (conf == true) {
-    window.location.assign("old/index.html");
-}
