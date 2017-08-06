073f08d625f0a83317c326a23bf13e4d7cdffb3d
Andre Reus <andrereus@users.noreply.github.com>
2017-08-06 20:55:40 +0200

    New test

--- js/login.js
+++ js/login.js
@@ -68,7 +68,7 @@ function initApp() {
 document.onreadystatechange = function() {
     if (document.readyState === "complete") {
         window.isphone = false;
-        if (document.URL.indexOf("http://") == -1 && document.URL.indexOf("localhost") != 7) {
+        if (document.URL.indexOf("http://") === -1 && document.URL.indexOf("https://") === -1) {
             window.isphone = true;
         }
 
