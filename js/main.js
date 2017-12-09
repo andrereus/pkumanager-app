9b9209f75cf8161b4b1030e98db41a5dd2d511c5
Andre Reus <andrereus@users.noreply.github.com>
2017-12-10 00:02:40 +0100

    Optimize dropdown & gif

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 /* Dropdown */
-$("#storage").on("click", function(){
-    $("#dropdown").toggleClass("hidden");
+var storage = $("#storage");
+var dropdown = $("#dropdown");
+
+storage.on("click", function(){
+    dropdown.toggleClass("hidden");
+});
+
+$(document).on("click", function(e){
+    if (!$(e.target).is(storage) && !$(e.target).is(dropdown)) {
+        dropdown.addClass("hidden");
+    }
 });
